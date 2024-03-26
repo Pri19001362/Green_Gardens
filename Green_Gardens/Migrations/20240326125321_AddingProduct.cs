de4865dce37aff272bc9eae7020d1149f265c907
Samuel Price <PRI19001362@btc.ac.uk>
2024-03-26 12:56:13 +0000

    Created customer ,admin, product and order tables

---
+++ Green_Gardens/Migrations/20240326125321_AddingProduct.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Green_Gardens.Migrations
+{
+    public partial class AddingProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Green_Gardens/Migrations/20240326125427_AddingOrder.Designer.cs
+++ Green_Gardens/Migrations/20240326125427_AddingOrder.Designer.cs
@@ -4,6 +4,7 @@ using Green_Gardens.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Green_Gardens.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240326125427_AddingOrder")]
+    partial class AddingOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
