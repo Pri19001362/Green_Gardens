52c5392e43843cd37635c160a7fa3ee614da9828
Samuel Price <PRI19001362@btc.ac.uk>
2024-03-26 12:00:49 +0000

    Updated program.cs page for database creation

--- Green_Gardens/Program.cs
+++ Green_Gardens/Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Green_Gardens.Model;
+using Green_Gardens.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Retrieve the database connection string labeled "DefaultConnection" from the application's configuration settings.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Register the DbContext for the application, specifying to use SQL Server with the connection string obtained earlier.
+// AppDbContext is the class managing the database context.
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(connectionString));
+//Register a singleton instance of product
+builder.Services.AddSingleton<Customer>();
+
+
+// Register services for MVC Controllers and Views. This is essential for applications using the MVC architecture.
+builder.Services.AddControllersWithViews();
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
