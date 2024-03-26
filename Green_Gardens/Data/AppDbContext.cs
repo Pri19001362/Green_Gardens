336c19ff6ce9f04c67c59f72549b61c3f12bd6c9
Samuel Price <PRI19001362@btc.ac.uk>
2024-03-26 11:46:52 +0000

    Created data folder with AppDbContext and added comments

---
+++ Green_Gardens/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Green_Gardens.Model;
+
+namespace Green_Gardens.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //DbSet represents the collection of all entities in the context, or that can be queried from the databse, of a given type.
+        public DbSet<Customer> Customer { get; set; }
+
+        //Constructor for the AppDbContext class
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //The base constructor handles the options. {Connection strings and Database Provider)
+        }
+    }
+}
