34213bb73db50867da742d355b76e127ffbba846
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-10 10:03:37 +0100

    Created edit page

---
+++ Green_Gardens/Pages/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_Gardens.Pages
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
