fd1923a7e51b1efdab2fbf16d8c4b6985d7da2d9
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-10 10:57:27 +0100

    Added code for delete option in delete.cs page

--- Green_Gardens/Pages/Delete.cshtml.cs
+++ Green_Gardens/Pages/Delete.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Green_Gardens.Data;
+using Green_Gardens.Model;
 
 namespace Green_Gardens.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext _dbConnection;
+
+        public Product Product { get; set; }
+
+        public DeleteModel(AppDbContext context)
         {
+            _dbConnection = context;
+        }
+
+        public void OnGet(Guid id)
+        {
+            // Retrieve the item to be deleted
+            Product = _dbConnection.Product.FirstOrDefault(t => t.Id == id);
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid id)
+        {
+            var itemToDelete = _dbConnection.Product.FirstOrDefault(t => t.Id == id);
+            if (itemToDelete != null)
+            {
+                _dbConnection.Product.Remove(itemToDelete);
+                await _dbConnection.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                // Handle the case where the item does not exist
+                return NotFound();
+            }
         }
     }
 }
