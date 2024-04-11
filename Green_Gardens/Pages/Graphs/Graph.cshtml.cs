152f2681a768a63c151e9ef9824d62c9fe81e64e
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-11 11:07:24 +0100

    Updated graphs c# script

--- Green_Gardens/Pages/Graphs/Graph.cshtml.cs
+++ Green_Gardens/Pages/Graphs/Graph.cshtml.cs
@@ -1,12 +1,28 @@
+using Green_Gardens.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
 
 namespace Green_Gardens.Pages.Graphs
 {
     public class GraphModel : PageModel
     {
+
+        private readonly AppDbContext _dbConnection;
+
+        public string TasksJson { get; set; }
+
+        public GraphModel(AppDbContext db)
+        {
+            _dbConnection = db;
+        }
+
+
         public void OnGet()
         {
+            var items = _dbConnection.Product.ToList();
+            TasksJson = JsonSerializer.Serialize(items.Select(t => new { Stock = t.Stock, t.Price }));
+
         }
     }
 }
