0df1799e73d0720278988a1fecda1cf4f1c0235b
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-11 11:20:31 +0100

    Add code to product c# script to display products

--- Green_Gardens/Pages/Products.cshtml.cs
+++ Green_Gardens/Pages/Products.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Green_Gardens.Data;
+using Green_Gardens.Model;
 
 namespace Green_Gardens.Pages
 {
     public class ProductsModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public List<Product> Products { get; set; }
+
+        public ProductsModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Products = _db.Product.ToList();
         }
     }
 }
