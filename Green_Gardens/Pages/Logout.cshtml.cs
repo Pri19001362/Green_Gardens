4a84cb6829b937d32de2ef48970e626615910de8
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-11 10:08:32 +0100

    Added functionality for what happens when logging out

--- Green_Gardens/Pages/Logout.cshtml.cs
+++ Green_Gardens/Pages/Logout.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,10 @@ namespace Green_Gardens.Pages
 {
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            await HttpContext.SignOutAsync();
+            return RedirectToPage("/Index");
         }
     }
 }
