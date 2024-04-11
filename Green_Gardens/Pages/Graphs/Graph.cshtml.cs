a677893f9968b25cbe0c2ccada28351d8e6cb6af
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-11 11:05:00 +0100

    Added Graph folder

---
+++ Green_Gardens/Pages/Graphs/Graph.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_Gardens.Pages.Graphs
+{
+    public class GraphModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
