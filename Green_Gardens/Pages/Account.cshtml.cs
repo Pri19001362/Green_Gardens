ce481664bc4efa4f54166c4f6d573eb65872bd81
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-19 10:36:08 +0100

    Updating account page

--- Green_Gardens/Pages/Account.cshtml.cs
+++ Green_Gardens/Pages/Account.cshtml.cs
@@ -1,3 +1,5 @@
+using Green_Gardens.Data;
+using Green_Gardens.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace Green_Gardens.Pages
 {
     public class AccountModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public List<Customer> Customer { get; set; }
+
+        public AccountModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Customer = _db.Customer.ToList();
         }
     }
 }
