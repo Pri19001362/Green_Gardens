1cebdd3e099f70e3b17889809af925cf7779612b
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-19 09:11:05 +0100

    created razor pages for account and added account button to the shop

---
+++ Green_Gardens/Pages/Account.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_Gardens.Pages
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
