a970b113758e795ff0fcc4d3358a3b8494d40f60
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-24 14:14:22 +0100

    added basket page and button

---
+++ Green_Gardens/Pages/Basket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_Gardens.Pages
+{
+    public class BasketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
