de330be9bc5b2cca0e55513673bde0e0ef04ef83
Samuel Price <PRI19001362@btc.ac.uk>
2024-04-24 14:16:27 +0100

    Updated backend of basket

--- Green_Gardens/Pages/Basket.cshtml.cs
+++ Green_Gardens/Pages/Basket.cshtml.cs
@@ -1,3 +1,5 @@
+using Green_Gardens.Data;
+using Green_Gardens.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,27 @@ namespace Green_Gardens.Pages
 {
     public class BasketModel : PageModel
     {
+        private readonly AppDbContext _db;
+
+        public List<Customer> Customer { get; set; }
+        public List<Order> Order { get; set; }
+        public List<Product> Products { get; set; }
+
+        public BasketModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Customer = _db.Customer.ToList();
+            var email = HttpContext.Session.GetString("UserEmail");
+            if (!string.IsNullOrEmpty(email))
+            {
+                Customer = _db.Customer.Where(c => c.Email == email).ToList();
+            }
+            Order = _db.Order.ToList();
+            Products = _db.Product.ToList();
         }
     }
 }
